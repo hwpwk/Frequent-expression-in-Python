5baf572280e6521740324cbd5a08b1f119ad47a2
hwpwk <34832156+hwpwk@users.noreply.github.com>
2018-11-18 17:14:55 +0900

    Add files via upload

---
+++ Jupyter notebookで行と列の表示の省略を防ぐコード.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+#最大表示行数の指定（ここでは100行を指定）
+pd.set_option('display.max_rows', 100)
+
+#最大表示列数の指定（ここでは100列を指定）
+pd.set_option('display.max_columns', 100)
+
+# 参考URL:https://qiita.com/daifuku_mochi2/items/30258e58750ff8e85d37
