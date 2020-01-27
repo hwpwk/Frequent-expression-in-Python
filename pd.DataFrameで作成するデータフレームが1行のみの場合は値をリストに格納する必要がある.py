d8bde64eb1f2c7f53b21f3038aafcef3b542562a
hwpwk <34832156+hwpwk@users.noreply.github.com>
2020-01-27 16:45:15 +0900

    金額合計':[pkl_df['金額'].sum()]

---
+++ pd.DataFrameで作成するデータフレームが1行のみの場合は値をリストに格納する必要がある.py
@@ -0,0 +1,9 @@
+﻿import pandas as pd
+
+# 作成するデータフレームが1行のみの場合は値をリストに格納する必要がある
+
+pkl_out_df = pd.DataFrame({
+    'データ件数':[pkl_df.shape[0]],
+    '金額合計':[pkl_df['金額'].sum()],
+    '単価合計':[pkl_df['単価'].sum()]
+})
