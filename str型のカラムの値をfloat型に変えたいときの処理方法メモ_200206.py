c3a55c0bd80a0182c7392c428cf9345d28b4ffb6
hwpwk <34832156+hwpwk@users.noreply.github.com>
2020-02-06 20:23:26 +0900

    lambda x: Decimal(x.replace(',','')) の後にastype

---
+++ str型のカラムの値をfloat型に変えたいときの処理方法メモ_200206.py
@@ -0,0 +1,32 @@
+import sys,os
+import gc
+import glob
+import pandas as pd
+
+files = glob.glob(r'L:/Work/20008_日立_SSB/2._Data/2.1_Received_Data/金融/csv/*WBS*')
+files = sorted(wbs_files)
+
+file_name_list = [os.path.splitext(os.path.basename(file))[0] for file in files]
+
+df_list = [pd.read_csv(file,usecols=['売上'],encoding='cp932', engine='python') for file in files]
+
+
+'''
+以降、[売上]カラムの値を算出したいが[売上]カラムの値の1つに「8,400.00」といったカンマが入ってしまっている場合を想定する。
+このとき、pandas.read_csvの引数に「dtype=float」と与えて[売上]カラムのデータ型をfloat型に変換しようとすると
+「unable to convert column to type class 'float'」というエラーが発生してしまう可能性や、
+[売上]カラムをstring型で読み込んだ後に
+df['売上'] = df['売上'].apply(lambda x: x.replace(',','').astype(float))
+という処理でデータ型をfloat型に変換しようとすると
+「'str' object has no attribute 'astype'」というエラーが発生してしまう可能性がある。
+このような場合には、str型からDecimal型にしてその後にfloat型に変換する処理を行う
+'''
+
+from decimal import Decimal
+
+df_list2 = []
+
+for df in wbs_df_list:
+    df['売上'] = df['売上'].apply(lambda x: Decimal(x.replace(',','')))
+    df['売上'] = df['売上'].astype(float)
+    df_list2.append(df)
