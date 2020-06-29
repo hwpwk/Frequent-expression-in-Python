56b7f7936d6b077c5ccde039404e337d9fe5a9b9
hwpwk <34832156+hwpwk@users.noreply.github.com>
2020-06-29 17:13:30 +0900

    df[np.isinf(df[value_col_list]).any(1)]

---
+++ データフレームから値がinfとなっているレコードを抽出.py
@@ -0,0 +1,9 @@
+﻿import pandas as pd
+import numpy as np
+
+
+# intやfloatなどの数値型のカラムのみ抽出
+value_col_list = tmp_df2.select_dtypes(include=['number']).columns.tolist()
+
+# データフレームから値がinfとなっているレコードを抽出
+df[np.isinf(df[value_col_list]).any(1)]
