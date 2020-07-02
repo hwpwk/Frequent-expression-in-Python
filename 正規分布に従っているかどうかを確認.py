02010ca79dcc48a02e04d7bf5dca6719cb1a8278
hwpwk <34832156+hwpwk@users.noreply.github.com>
2020-07-02 13:42:41 +0900

    W, p= stats.shapiro(df['x']) print( 'p値 = ', p)

---
+++ 正規分布に従っているかどうかを確認.py
@@ -0,0 +1,15 @@
+﻿import pandas as pd
+import numpy as np
+
+# ヒストグラムの描画
+df['_正規化'].hist()
+
+# 正規Q-Qプロットによる可視化
+from scipy import stats
+stats.probplot(df['_正規化'], dist='norm', plot=plt)
+
+# シャピロウィルク検定(欠損値があるとWがnan、P値が1になるので欠損値を補完してから使用する)
+from scipy import stats
+W, p = stats.shapiro(df['_正規化'])
+print( 'p値 = ', p)
+
