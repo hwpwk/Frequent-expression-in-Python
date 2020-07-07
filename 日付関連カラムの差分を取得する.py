6e5b878b7b1fd0fefbbdbb56cd87dce291279d88
hwpwk <34832156+hwpwk@users.noreply.github.com>
2020-07-08 07:57:03 +0900

    (df['決算日']-df['開始日']).dt.days

---
+++ 日付関連カラムの差分を取得する.py
@@ -0,0 +1,10 @@
+﻿import pandas as pd
+import numpy as np
+
+# 下記3つのカラムをdatetime64型に変換
+date_col = ['作業開始年月日','決算年月日', '引渡年月日']
+for col in date_col:
+    df[col] = pd.to_datetime(df[col])
+
+# 日付の差分を取得
+df['diff'] = (df['決算年月日']-df['作業開始年月日(商談単位の最小)']).dt.days
