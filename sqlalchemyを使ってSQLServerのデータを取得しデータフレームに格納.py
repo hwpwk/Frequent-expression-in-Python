84181a22213f713ef23b32b6b3ca07ba1effa145
hwpwk <34832156+hwpwk@users.noreply.github.com>
2020-07-27 07:39:28 +0900

    df=pd.read_sql(query, con=conn)

---
+++ sqlalchemyを使ってSQLServerのデータを取得しデータフレームに格納.py
@@ -0,0 +1,17 @@
+﻿import sqlalchemy
+from sqlalchemy import create_engine
+import urllib
+
+dsn = 'Driver={SQL Server Native Client 11.0};Server=xxxx;Database=database100;trusted_connection=Yes'
+dsn = urllib.parse.quote_plus(dsn)
+engine = sqlalchemy.create_engine(f'mssql+pyodbc:///?odbc_connect={dsn}')
+conn = engine.connect()
+
+query='''
+SELECT 
+      [コード]
+      ,[決算月]
+FROM
+      [dbo].[Table_All]
+'''
+df = pd.read_sql(query, con=conn)
