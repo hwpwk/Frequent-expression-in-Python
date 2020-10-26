8eac98042ddd8b77974cc1b26e967ee88dc194e0
hwpwk <34832156+hwpwk@users.noreply.github.com>
2020-10-26 12:07:33 +0900

    [nam for nam in name_list if nam not in del_list]

---
+++ list(リスト)の要素の複数削除.py
@@ -0,0 +1,4 @@
+﻿del_fs_name_list = ['KK_FS_201403','KK_FS_202012']
+
+# name_listに含まれている要素がdel_fs_name_listに含まれていれば、その要素は抽出しない
+fs_name_list2 = [name for name in fs_name_list if name not in del_fs_name_list]
